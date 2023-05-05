8eac12bdc866c12d397b9d3f41d2e4a2e5b474da
Radiy <108869069+notron124@users.noreply.github.com>
2023-05-05 17:10:21 +0500

    v3
    New way better version

---
+++ DebounceAvoid/debounceAvoid.c
@@ -0,0 +1,72 @@
+#include "debounceAvoid.h"
+
+/*Header with your flags, we need KEY_READY flags*/
+#include "common.h"
+
+/*Header with your global variables, for KEY_READY flags, SND macro*/
+#include "glob.h"
+
+/*Header with ports definition, we need SND_ON/OFF macro*/
+#include "port.h"
+
+uint16_t keyFlags;
+uint16_t autorepeatFlags;
+uint8_t keyCode;
+
+uint8_t commonKeyFlags;
+
+void DebounceAvoid(struct Key_TypeDef *keyx)
+{
+   if (!(keyx->GPIOx->IDR & keyx->pin))
+   {
+      keyx->counter++;
+
+      if (keyx->longPressDelay != 0)
+      {
+         if (keyx->counter == keyx->longPressDelay) // first wait for longpress
+         {
+            if (keyx->flags.autorepeat == 1)
+            {
+#ifdef SND_DOUBLE_ON
+               if (!KEY_AUTO_REPEAT)
+                     SND_DOUBLE_ON;
+#endif
+               keyx->counter  = keyx->longPressDelay - 50;
+            }
+            else
+            {
+#ifndef SND_LONG_ON
+#ifdef SND_ON
+               SND_ON;
+#endif
+#else
+               SND_LONG_ON;
+#endif
+            }
+
+            keyCode = keyx->longPressID;
+            SET_KEY_READY;
+            keyx->flags.isPressed = 1;
+         }
+      }
+   }
+   else
+   {
+      if (((keyx->counter >= keyx->shortPressDelay) && !(keyx->flags.isPressed))) // if key is unpressed, check if time pressed is enough
+      {
+         keyx->counter = 0;
+         keyCode = keyx->ID;
+         SET_KEY_READY;
+#ifdef SND_ON
+         SND_ON;
+#endif
+         keyx->flags.isPressed = 1;
+      }
+      else // if not enough
+      {
+         //RESET_KEY_AUTO_REPEAT;
+         keyx->counter = 0;
+         keyx->flags.isPressed = 0;
+      }
+   }
+}
---
+++ DebounceAvoid/debounceAvoid.h
@@ -0,0 +1,39 @@
+#ifndef _DEBOUNCE_AVOID_H_
+#define _DEBOUNCE_AVOID_H_
+
+/*Your main header file*/
+#include "stm8s.h"
+
+struct Key_TypeDef
+{
+   GPIO_TypeDef *GPIOx;
+   uint16_t pin;
+   uint16_t counter;
+   uint8_t shortPressDelay;
+   uint16_t longPressDelay;
+   uint8_t ID;
+   uint8_t longPressID;
+   struct {
+      uint8_t autorepeat : 1;
+      uint8_t isPressed : 1;
+   } flags;
+};
+
+
+
+extern uint16_t autorepeatFlags;
+extern uint8_t keyCode;
+
+extern uint8_t commonKeyFlags;
+
+/*#define F_KEY_READY     0x01
+
+#define SET_KEY_READY   commonKeyFlags |= F_KEY_READY
+#define RESET_KEY_READY commonKeyFlags &= ~F_KEY_READY
+#define KEY_READY       (commonKeyFlags & F_KEY_READY)*/
+
+
+
+void DebounceAvoid(struct Key_TypeDef *keyx);
+
+#endif
